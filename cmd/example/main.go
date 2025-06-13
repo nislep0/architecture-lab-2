296496301f049a85b4ae07b10bcd140932b97ce6
kwadi <ruden1603@gmail.com>
2025-06-13 19:19:49 +0300

    update main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,25 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"io"
+	"os"
+	"strings"
+
+	lab2 "github.com/nislep0/architecture-lab-2"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	exprFlag   = flag.String("e", "", "Expression to compute")
+	fileFlag   = flag.String("f", "", "Path to input file")
+	outputFlag = flag.String("o", "", "Path to output file")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if (*exprFlag != "" && *fileFlag != "") || (*exprFlag == "" && *fileFlag == "") {
+		fmt.Fprintln(os.Stderr, "Error: must provide either -e or -f, but not both or neither.")
+		os.Exit(1)
+	}
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	var inputReader io.Reader
+	if *exprFlag != "" {
+		inputReader = strings.NewReader(*exprFlag + "\n")
+	} else {
+		file, err := os.Open(*fileFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening input file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		inputReader = file
+	}
+
+	var outputWriter io.Writer
+	if *outputFlag != "" {
+		file, err := os.Create(*outputFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating output file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		outputWriter = file
+	} else {
+		outputWriter = os.Stdout
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  inputReader,
+		Output: outputWriter,
+	}
+
+	err := handler.Compute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Computation error: %v\n", err)
+		os.Exit(1)
+	}
 }
